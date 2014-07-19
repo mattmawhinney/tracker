c6a7601c8816eaa54cf901eaa95c5ab104184677
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-07-19 13:37:53 -0400

    generated Deliverable api

---
+++ api/controllers/DeliverableController.js
@@ -0,0 +1,11 @@
+/**
+ * DeliverableController
+ *
+ * @description :: Server-side logic for managing deliverables
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
