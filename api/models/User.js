99e0e41754ef7f5703722c1e086becb5275c78ee
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-07-19 14:41:00 -0400

    updated notes on associations in User.js

--- api/models/User.js
+++ api/models/User.js
@@ -7,6 +7,7 @@
  */
 
  //Associations: https://github.com/balderdashy/waterline-docs/blob/master/associations.md
+ // https://github.com/balderdashy/sails/issues/124
  //has many projects
  //has many deliverable through projects 
 
